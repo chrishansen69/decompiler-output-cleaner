602b5eaa7357b917eed6ba513a47257df750363c
chrishansen69 <cadrekucra@gmx.com>
2016-04-07 17:30:19 -0300

    add jfilechooser

--- src/FernCleaner.java
+++ src/FernCleaner.java
@@ -1,9 +1,21 @@
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.swing.JFileChooser;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseException;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.MethodDeclaration;
@@ -16,9 +28,28 @@ import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 public class FernCleaner {
 
-    public static void main(final String[] args) throws Exception {
+    public static void main(final String[] args) throws FileNotFoundException, ParseException, IOException {
 
-        final FileInputStream in = new FileInputStream("F51.java");
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+        }
+
+        final JFileChooser chooser = new JFileChooser();
+        final FileNameExtensionFilter filter = new FileNameExtensionFilter("Java source code", "java");
+        chooser.setFileFilter(filter);
+        chooser.setMultiSelectionEnabled(true);
+        final int returnVal = chooser.showOpenDialog(null);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            for (final File f : chooser.getSelectedFiles()) {
+                writeToFile(f);
+            }
+        }
+
+    }
+
+    private static void writeToFile(final File f) throws FileNotFoundException, ParseException, IOException {
+        final FileInputStream in = new FileInputStream(f);
 
         CompilationUnit cu;
         try {
@@ -32,8 +63,12 @@ public class FernCleaner {
         new MethodChangerVisitor().visit(cu, null);
 
         // prints the changed compilation unit
-        System.out.println(cu.toString());
+        //System.out.println(cu.toString());
 
+        f.delete();
+        final BufferedWriter pw = new BufferedWriter(new PrintWriter(new FileOutputStream(f)));
+        pw.write(cu.toString());
+        pw.close();
     }
 
     private static class MethodChangerVisitor extends VoidVisitorAdapter<Object> {
@@ -42,9 +77,9 @@ public class FernCleaner {
 
         private final HashMap<String, String> variablesMap = new HashMap<>();
         private final HashMap<String, Integer> lastVarIndex = new HashMap<>();
-        
+
         private boolean inMethod = false;
-        
+
         private final static String[] i_types = {
                 "i", "j", "k", "l"
         };
@@ -62,7 +97,7 @@ public class FernCleaner {
 
             final List<Parameter> params = new ArrayList<>(n.getParameters());
 
-            for (Parameter param : params) {
+            for (final Parameter param : params) {
                 final VariableDeclaratorId varDec = param.getId(); //var name
                 System.out.println("type of parameter: " + param.getType()); //turns out this works, since it extends node which is... just a blob of text... who would have guessed
 
@@ -81,17 +116,17 @@ public class FernCleaner {
 
         @Override
         public void visit(final VariableDeclarationExpr n, final Object arg) {
-            
+
             if (!inMethod)
                 return;
-            
+
             // this needs to be called otherwise it fucks up all other method calls
             super.visit(n, arg);
 
             //System.out.println("got to expr");
             final List<VariableDeclarator> vars = n.getVars();
 
-            for (VariableDeclarator varHandle : vars) {
+            for (final VariableDeclarator varHandle : vars) {
                 final VariableDeclaratorId varDec = varHandle.getId(); //var name
                 final String varName = varDec.getName();
                 System.out.println("declarated name: " + varName);
@@ -121,7 +156,7 @@ public class FernCleaner {
 
         /**
          * Gives you an unique and nifty variable name and adds it to variablesMap if it's not there already.
-         * 
+         *
          * @param n The node containing the variable.
          * @param varName The variable's name (redundant, FIXME)
          * @return an unique and nifty variable name
@@ -132,7 +167,7 @@ public class FernCleaner {
 
         /**
          * Gives you an unique and nifty variable name and adds it to variablesMap if it's not there already.
-         * 
+         *
          * @param n The node containing the variable.
          * @param varName The variable's name (redundant, FIXME)
          * @return an unique and nifty variable name
@@ -143,7 +178,7 @@ public class FernCleaner {
 
         /**
          * Gives you an unique and nifty variable name and adds it to variablesMap if it's not there already.
-         * 
+         *
          * @param n The node containing the variable.
          * @param varName The variable's name (redundant, FIXME)
          * @param type the output of <code>n.getType().toString()</code>, required for reasons
@@ -154,36 +189,37 @@ public class FernCleaner {
             if (a == null) {
                 a = fixArray(getTypeAndIncrement(type));
                 variablesMap.put(varName, a);
-            }/* else {
-                lastVarIndex.put(type, lastVarIndex.get(type) + 1);
-            }*/
+            } /* else {
+                 lastVarIndex.put(type, lastVarIndex.get(type) + 1);
+              }*/
             return a;
         }
 
         /**
          * Fixes a string containing []
+         * 
          * @param a string
          * @return a fixed string
          */
-        private static String fixArray(String a) {
+        private static String fixArray(final String a) {
             return a.replaceFirst("\\[\\]", "s").replace("[]", ""); //careful with array types when adding better numbering system because of 2-dimensional arrays
         }
 
         private String getTypeAndIncrement(final String type) {
             incrementIfNotExists(type);
-            
-            String varIndex = getVarIndex(lastVarIndex.get(type), type.toLowerCase());
-            
-            String a = varIndex;
+
+            final String varIndex = getVarIndex(lastVarIndex.get(type), type.toLowerCase());
+
+            final String a = varIndex;
             return a;
         }
-        
-        private String getVarIndex(int varIndex, String type) {
+
+        private String getVarIndex(final int varIndex, final String type) {
             if (type.equals("int")) {
                 if (varIndex <= i_types.length)
                     return i_types[varIndex - 1];
                 else
-                    return i_types[(varIndex - 1) % i_types.length] + (varIndex / i_types.length);
+                    return i_types[(varIndex - 1) % i_types.length] + varIndex / i_types.length;
             } else if (varIndex == 1) {
                 if (type.equals("byte"))
                     return "b";
@@ -214,11 +250,12 @@ public class FernCleaner {
         }
 
         private void incrementIfNotExists(final String type) {
-            Integer vType = lastVarIndex.get(type);
-            if (vType != null)
+            final Integer vType = lastVarIndex.get(type);
+            if (vType != null) {
                 lastVarIndex.put(type, vType + 1);
-            else
+            } else {
                 lastVarIndex.put(type, 1);
+            }
         }
     }
 }
